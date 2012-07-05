26ed97afcb009417a1a4c4915d6d353d1f32fafe
Kirill Pinchuk <k_pinchuk@wargaming.net>
2012-07-05 14:37:34 +0300

    Add smex to init

--- init.el
+++ init.el
@@ -15,6 +15,21 @@
 (require 'autopair)
 (require 'paredit)
 (require 'espresso)
+(require 'smex)
+;; smex delayed initialization
+(global-set-key [(meta x)] (lambda ()
+                             (interactive)
+                             (or (boundp 'smex-cache)
+                                 (smex-initialize))
+                             (global-set-key [(meta x)] 'smex)
+                             (smex)))
+
+(global-set-key [(shift meta x)] (lambda ()
+                                   (interactive)
+                                   (or (boundp 'smex-cache)
+                                       (smex-initialize))
+                                   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+                                   (smex-major-mode-commands)))
 
 (setq erlang-root-dir "/usr/lib/erlang")
 (setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
