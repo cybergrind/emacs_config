eef79b84ffb2a6f5934d171afb5ace26a3f8a55d
Kirill Pinchuk <k_pinchuk@wargaming.net>
2013-05-23 16:03:23 +0300

    Fix flymake

--- flymake_cust.el
+++ flymake_cust.el
@@ -11,12 +11,12 @@
   (defun flymake-pyflakes-init ()
      ; Make sure it's not a remote buffer or flymake would not work
      (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                    'flymake-create-temp-in-system-tempdir))
-             (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "pyflakes" (list temp-file)))))
+       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                              'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+    (list "pyflakes" (list local-file)))))
 
 
   (add-to-list 'flymake-allowed-file-name-masks
