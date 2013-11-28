7656da69d6125c613f80d116eed82dc0e4decc23
Kirill Pinchuk <k_pinchuk@wargaming.net>
2013-11-28 18:04:08 +0300

    Change jumps to registers

--- key_chord_setup.el
+++ key_chord_setup.el
@@ -6,8 +6,8 @@
 (key-chord-define-global "JJ" 'switch-to-prev-buffer)
 (key-chord-define-global "KK" 'switch-to-next-buffer)
 (key-chord-define-global "uu" 'undo-tree-visualize)
-(key-chord-define-global "fj" 'bookmark-jump)
-(key-chord-define-global "yt" 'bookmark-set)
+(key-chord-define-global "fj" 'jump-to-register)
+(key-chord-define-global "ty" 'point-to-register)
 (key-chord-define-global "vm" 'bookmark-bmenu-list)
 
 (key-chord-mode +1)
