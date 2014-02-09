4d58cc48fa4a992d15fe0e4cf461c62f41dccc89
Kirill Pinchuk <k_pinchuk@wargaming.net>
2014-02-09 23:58:17 +0300

    kk disable

--- key_chord_setup.el
+++ key_chord_setup.el
@@ -1,17 +1,17 @@
-(require 'key-chord)
+;(require 'key-chord)
+;
+;(setq key-chord-two-keys-delay 0.05)
+;(setq key-chord-one-key-delay 0.2)
+;
+;(key-chord-define-global "jj" 'ace-jump-word-mode)
+;(key-chord-define-global "jl" 'ace-jump-line-mode)
+;(key-chord-define-global "jk" 'ace-jump-char-mode)
+;(key-chord-define-global "JJ" 'switch-to-prev-buffer)
+;(key-chord-define-global "KK" 'switch-to-next-buffer)
+;(key-chord-define-global "fj" 'jump-to-register)
+;(key-chord-define-global "fy" 'point-to-register)
+;(key-chord-define-global "vm" 'bookmark-bmenu-list)
 
-(setq key-chord-two-keys-delay 0.05)
-(setq key-chord-one-key-delay 0.2)
+;(key-chord-mode +1)
 
-(key-chord-define-global "jj" 'ace-jump-word-mode)
-(key-chord-define-global "jl" 'ace-jump-line-mode)
-(key-chord-define-global "jk" 'ace-jump-char-mode)
-(key-chord-define-global "JJ" 'switch-to-prev-buffer)
-(key-chord-define-global "KK" 'switch-to-next-buffer)
-(key-chord-define-global "fj" 'jump-to-register)
-(key-chord-define-global "fy" 'point-to-register)
-(key-chord-define-global "vm" 'bookmark-bmenu-list)
-
-(key-chord-mode +1)
-
-(provide 'key_chord_setup)
+;(provide 'key_chord_setup)
