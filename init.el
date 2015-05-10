bd1ec8f5349f17d360eb46d476ce3fa673d76684
Kirill Pinchuk <k_pinchuk@wargaming.net>
2015-05-10 12:47:22 +0300

    Add jsx-mode

--- init.el
+++ init.el
@@ -284,3 +284,6 @@
 
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
