68babc39286c1a21de087a59c04c7da1821e54a3
Kirill Pinchuk <k_pinchuk@wargaming.net>
2015-08-07 17:16:33 +0300

    Update agenda
    Signed-off-by: Kirill Pinchuk <k_pinchuk@wargaming.net>

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
  '(nrepl-port 9999)
  '(org-agenda-files
    (quote
-    ("~/Dropbox/gtd/gtd.org" "~/Dropbox/gtd/logbook/14_09.org" "~/Dropbox/gtd/calendar.org_archive" "~/Dropbox/gtd/calendar.org")))
+    ("~/Dropbox/gtd/tipsi.org" "~/Dropbox/gtd/gtd.org" "~/Dropbox/gtd/logbook/14_09.org" "~/Dropbox/gtd/calendar.org_archive" "~/Dropbox/gtd/calendar.org")))
  '(org-directory "~/Dropbox/gtd")
  '(org-mobile-directory "~/Dropbox/gtd")
  '(org-mobile-inbox-for-pull "~/Dropbox/gtd/mobileorg.org")
@@ -315,3 +315,24 @@
 ; macro
 (fset 'fix-indent
       "\C-i\C-n\-m\C-x(")
+
+; hydra
+
+(require 'hydra)
+
+(defhydra cqql-multiple-cursors-hydra (:hint nil)
+    "
+     ^Up^            ^Down^        ^Miscellaneous^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("q" nil))
