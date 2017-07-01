8d167d3326f34f5d3cba22cd60badb0d4e8f7cb8
Kirill Pinchuk <cybergrind@gmail.com>
2017-07-01 19:22:32 +0300

    python_setup: fix linter warnings

--- lisp/python_setup.el
+++ lisp/python_setup.el
@@ -1,4 +1,9 @@
-(require 'flycheck) ; noqa
+;;; python_setup.py --- python specific code
+;;; Commentary:
+;;; Code:
+
+(require 'flycheck)
+(require 'python)
 
 ; Python checker
 (add-hook 'python-mode-hook
@@ -9,6 +14,7 @@
 (defvar py-test-name "")
 
 (defun run-py-test ()
+  "Execute test and print result."
   (cond
    ((and (boundp 'py-project-root)
          (boundp 'py-test-command)
@@ -20,20 +26,42 @@
                py-test-name))))
         (t (message "Please set py-project-root or chose test"))))
 
+
+(defun get-path-pytest ()
+  "In pytest format: path/to/file.py::function_name ."
+  (let* ((curr_test (python-info-current-defun))
+         (test_path (concat (buffer-file-name) "::" curr_test)))
+    test_path))
+
+(defun get-path-django-runner ()
+  "In django format: path_to_module.with_dots.ClassName.test_name ."
+  (let* ((curr_test (python-info-current-defun))
+         (curr_file (replace-regexp-in-string ".py$" "" (buffer-file-name)))
+         (curr_base (cadr (split-string curr_file py-project-root)))
+         (with_dots (replace-regexp-in-string "/" "." curr_base))
+         (test_path (concat with_dots "." curr_test)))
+    test_path))
+
+(defun get-py-test-path ()
+  "Get test path, respect test runner syntax."
+  (cond ((and (boundp 'py-test-runner)
+              (equal py-test-runner 'pytest))
+         (get-path-pytest))
+        (t
+         (get-path-django-runner))))
+
 (defun assign-py-test ()
+  "Assign test."
   (cond ((boundp 'py-project-root)
-         (let* ((curr_test (python-info-current-defun))
-                (curr_file (replace-regexp-in-string ".py$" "" (buffer-file-name)))
-                (curr_base (cadr (split-string curr_file py-project-root)))
-                (with_dots (replace-regexp-in-string "/" "." curr_base))
-                (test_path (concat with_dots "." curr_test)))
-           (message "Test path %s" test_path)
-           (setq py-test-name test_path)
+         (let ((test-path (get-py-test-path)))
+           ; (message "!!TEST PATH: %s" test-path)
+           (setq py-test-name test-path)
            (run-py-test)))
         (t (message "Please set py-project-root"))))
 
 
 (defun py-test-interactive (arg)
+  "Set and run python test.  ARG - if need to set test."
   (interactive "P")
   (if arg (assign-py-test))
   (run-py-test))
@@ -42,7 +70,6 @@
 (add-hook 'python-mode-hook
           #'(lambda ()
               (setq python-shell-interpreter "ipython")
-              ;; (setq python-shell-interpreter-args "--pylab")
               (define-key python-mode-map (kbd "C-o") 'py-test-interactive)
               (define-key python-mode-map (kbd "C-c .") 'goto-last-change)
               ;(define-key python-mode-map (kbd "DEL") 'py-electric-backspace)
@@ -50,3 +77,5 @@
           ))
 
 (provide 'python_setup)
+
+;;; python_setup.el ends here
