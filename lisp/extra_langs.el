c2aa579bcb89ed67cdee8e728f0db89222c621f1
Kirill Pinchuk <cybergrind@gmail.com>
2022-08-05 16:37:07 +0300

    julia support

--- lisp/extra_langs.el
+++ lisp/extra_langs.el
@@ -64,6 +64,42 @@
   :config
   (add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
 
+(use-package julia-mode
+  :mode ("\\.jl" . julia-mode))
+
+(use-package lsp-julia
+  :config
+  (setq lsp-julia-default-environment "~/.julia/environments/v1.7"))
+
+(use-package vterm
+  :ensure t)
+
+(defun my/julia-repl-send-cell()
+  ;; "Send the current julia cell (delimited by ###) to the julia shell"
+  (interactive)
+  (save-excursion (setq cell-begin (if (re-search-backward "^###" nil t) (point) (point-min))))
+  (save-excursion (setq cell-end (if (re-search-forward "^###" nil t) (point) (point-max))))
+  (set-mark cell-begin)
+  (goto-char cell-end)
+  (julia-repl-send-region-or-line)
+  (next-line))
+
+(use-package julia-repl
+  :ensure t
+  :hook (julia-mode . julia-repl-mode)
+
+  :init
+  (setenv "JULIA_NUM_THREADS" "8")
+
+  :config
+  ;; Set the terminal backend
+  (julia-repl-set-terminal-backend 'vterm)
+
+  ;; Keybindings for quickly sending code to the REPL
+  (define-key julia-repl-mode-map (kbd "<C-RET>") 'my/julia-repl-send-cell)
+  (define-key julia-repl-mode-map (kbd "<M-RET>") 'julia-repl-send-line)
+  (define-key julia-repl-mode-map (kbd "<S-return>") 'julia-repl-send-buffer))
+
 
 ;; (cond ((file-exists-p "/usr/lib/erlang")
 ;;        (setq erlang-root-dir "/usr/lib/erlang")
