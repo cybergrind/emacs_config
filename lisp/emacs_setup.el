6ec6c5f6264cc3b72cd4949717827bbd6bb466f9
Kirill Pinchuk <cybergrind@gmail.com>
2024-10-15 12:57:15 +0300

    evil: initial setup

--- lisp/emacs_setup.el
+++ lisp/emacs_setup.el
@@ -380,6 +380,17 @@
   :custom
   (combobulate-key-prefix "C-c o"))
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 (require 'kpi_multiple_cursors)
 (require 'init-ivy)
