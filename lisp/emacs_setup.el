3dc2741b8c62084b8f82478fc3f2417f9eb961f5
Kirill Pinchuk <cybergrind@gmail.com>
2024-10-17 18:10:21 +0300

    evil: enable fine undo

--- lisp/emacs_setup.el
+++ lisp/emacs_setup.el
@@ -38,6 +38,7 @@
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
+  (setq evil-want-fine-undo t)
   :config
   (evil-mode 1)
   (define-key evil-motion-state-map "," my-leader-map)
