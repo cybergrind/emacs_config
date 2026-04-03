682cfb32f2b27dc87f500ec4d0209ca968dc12af
Kirill Pinchuk <cybergrind@gmail.com>
2026-04-03 15:23:03 +0300

    wl-copy: dynamic WAYLAND_DISPLAY set

--- lisp/x-clipboard.el
+++ lisp/x-clipboard.el
@@ -1,6 +1,14 @@
+(defun x-clipboard--ensure-wayland-display ()
+  "Ensure WAYLAND_DISPLAY is set, detecting the socket if needed."
+  (unless (getenv "WAYLAND_DISPLAY")
+    (let ((socket (car (directory-files "/run/user/1000/" nil "^wayland-[0-9]+$"))))
+      (when socket
+        (setenv "WAYLAND_DISPLAY" socket)))))
+
 (defun x-paste ()
   "insert text on X11's clipboard to current buffer."
   (interactive)
+  (x-clipboard--ensure-wayland-display)
   (let ((input-method-function nil)
         ;; xsel -o
         (str (shell-command-to-string "wl-paste")))
@@ -9,6 +17,7 @@
 (defun x-copy ()
   "copy text on local kill-ring to X11's clipboard."
   (interactive)
+  (x-clipboard--ensure-wayland-display)
   (copy-region-as-kill (point) (mark t))
   (let ((process-connection-type nil))
     ;; "xsel" "-i" "-b"
